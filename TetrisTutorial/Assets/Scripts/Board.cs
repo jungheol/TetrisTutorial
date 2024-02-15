df6e5710d7353cd71622faa27165e16fe8018272
jungheol <jungheol@naver.com>
2024-02-15 20:23:05 +0900

    Board에서 Tetromino Init

--- TetrisTutorial/Assets/Scripts/Board.cs
+++ TetrisTutorial/Assets/Scripts/Board.cs
@@ -1,7 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
 public class Board : MonoBehaviour {
-	public TetrominoData[] tetrominoes; 
+	public TetrominoData[] tetrominoes;
+	public Tilemap tilemap { get; private set; }
+
+	private void Awake() {
+		this.tilemap = GetComponentInChildren<Tilemap>();
+		
+		for (int i = 0; i < this.tetrominoes.Length; i++) {
+			this.tetrominoes[i].Init();
+		}
+	}
+
+	private void Start() {
+		SpawnPiece();
+	}
+
+	public void SpawnPiece() {
+		int ran = Random.Range(0, tetrominoes.Length);
+		TetrominoData data = this.tetrominoes[ran];
+	}
+
+	public void Set() {
+		
+	}
 }
