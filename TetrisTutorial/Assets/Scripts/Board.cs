093515903d96b6c2bc45d5fb4179226639cbc773
jungheol <jungheol@naver.com>
2024-02-15 20:38:55 +0900

    Tetromino가 랜덤으로 생성되도록 구현

--- TetrisTutorial/Assets/Scripts/Board.cs
+++ TetrisTutorial/Assets/Scripts/Board.cs
@@ -4,11 +4,15 @@ using UnityEngine.Tilemaps;
 using Random = UnityEngine.Random;
 
 public class Board : MonoBehaviour {
+	
 	public TetrominoData[] tetrominoes;
 	public Tilemap tilemap { get; private set; }
+	public Piece activePiece { get; private set; }
+	public Vector3Int spawnPosition;
 
 	private void Awake() {
 		this.tilemap = GetComponentInChildren<Tilemap>();
+		this.activePiece = GetComponentInChildren<Piece>();
 		
 		for (int i = 0; i < this.tetrominoes.Length; i++) {
 			this.tetrominoes[i].Init();
@@ -22,9 +26,15 @@ public class Board : MonoBehaviour {
 	public void SpawnPiece() {
 		int ran = Random.Range(0, tetrominoes.Length);
 		TetrominoData data = this.tetrominoes[ran];
+		
+		this.activePiece.Init(this, this.spawnPosition, data);
+		Set(this.activePiece);
 	}
 
-	public void Set() {
-		
+	public void Set(Piece piece) {
+		for (int i = 0; i < piece.cells.Length; i++) {
+			Vector3Int tilePosition = piece.cells[i] + piece.position;
+			this.tilemap.SetTile(tilePosition, piece.data.tile);
+		}
 	}
 }
