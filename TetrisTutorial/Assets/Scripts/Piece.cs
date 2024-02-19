73e3a45b6053e410e9d9af224cb8b9707741c482
jungheol <jungheol@naver.com>
2024-02-19 17:09:30 +0900

    로테이션 기능 추가

--- TetrisTutorial/Assets/Scripts/Piece.cs
+++ TetrisTutorial/Assets/Scripts/Piece.cs
@@ -6,11 +6,13 @@ public class Piece : MonoBehaviour {
 	public TetrominoData data { get; private set; }
 	public Vector3Int[] cells { get; private set; }
 	public Vector3Int position { get; private set; }
+	public int rotationIndex;
 	
 	public void Init(Board board, Vector3Int position, TetrominoData data) {
 		this.board = board;
 		this.position = position;
 		this.data = data;
+		this.rotationIndex = 0;
 
 		if (this.cells == null) {
 			this.cells = new Vector3Int[data.cells.Length];
@@ -23,6 +25,10 @@ public class Piece : MonoBehaviour {
 
 	private void Update() {
 		this.board.Clear(this);
+
+		if (Input.GetKeyDown(KeyCode.W)) {
+			Rotate();
+		}
 		
 		if (Input.GetKeyDown(KeyCode.A)) {
 			Move(Vector2Int.left);
@@ -59,5 +65,36 @@ public class Piece : MonoBehaviour {
 		}
 
 		return valid;
+	}
+
+	private void Rotate() {
+		this.rotationIndex = Wrap(this.rotationIndex++, 0, 4);
+		float[] matrix = Data.RotationMatrix;
+
+		for (int i = 0; i < this.cells.Length; i++) {
+			Vector3 cell = this.cells[i];
+
+			int x, y;
+
+			switch (this.data.tetromino) {
+				case Tetromino.I:
+				case Tetromino.O:
+					cell.x -= 0.5f;
+					cell.y -= 0.5f;
+					x = Mathf.CeilToInt(cell.x * matrix[0] + cell.y * matrix[1]);
+					y = Mathf.CeilToInt(cell.x * matrix[2] + cell.y * matrix[3]);
+					break;
+				default:
+					x = Mathf.RoundToInt(cell.x * matrix[0] + cell.y * matrix[1]);
+					y = Mathf.RoundToInt(cell.x * matrix[2] + cell.y * matrix[3]);
+					break;
+			}
+
+			this.cells[i] = new Vector3Int(x, y, 0);
+		}
+	}
+
+	private int Wrap(int input, int min, int max) {
+		return min + (input - min) % (max - min);
 	}
 }
