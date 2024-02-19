decb1d522f60a947446a8e999bf6d5eacf3f5b57
jungheol <jungheol@naver.com>
2024-02-19 15:07:26 +0900

    Tetromino 좌우 움직임 구현

--- TetrisTutorial/Assets/Scripts/Piece.cs
+++ TetrisTutorial/Assets/Scripts/Piece.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Piece : MonoBehaviour {
@@ -21,5 +19,31 @@ public class Piece : MonoBehaviour {
 		for (int i = 0; i < data.cells.Length; i++) {
 			this.cells[i] = (Vector3Int)data.cells[i];
 		}
+	}
+
+	private void Update() {
+		this.board.Clear(this);
+		
+		if (Input.GetKeyDown(KeyCode.A)) {
+			Move(Vector2Int.left);
+		} else if (Input.GetKeyDown(KeyCode.D)) {
+			Move(Vector2Int.right);
+		}
+		
+		this.board.Set(this);
+	}
+
+	private bool Move(Vector2Int translation) {
+		Vector3Int newPosition = this.position;
+		newPosition.x += translation.x;
+		newPosition.y += translation.y;
+
+		bool valid = this.board.IsValidPos(this, newPosition);
+
+		if (valid) {
+			this.position = newPosition;
+		}
+
+		return valid;
 	}
 }
